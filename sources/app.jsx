333c13e800b79e9b4b1aaf2cf4779596d0c69c73
RennyKa <nguyenduclong.hy.96@gmail.com>
2021-10-07 09:13:31 +0700

    first commit

---
+++ sources/app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+
+export default hot(module)(function () {
+   return <div>App here 123456</div>;
+})
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+   mode: "development",
+   entry: path.join(__dirname, "sources/index.js"),
+   output: {
+      path: path.resolve(__dirname, "bundles"),
+      filename: "my-bundle.js",
+   },
+   module: {
+      rules: [
+         {
+            test: /\.?js$|jsx/,
+            exclude: /node_modules/,
+            use: {
+               loader: "babel-loader",
+               options: {
+                  presets: ["@babel/preset-env", "@babel/preset-react"],
+               },
+            },
+         },
+         {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+         },
+         {
+            test: /\.(png|jp(e*)g|svg|gif)$/,
+            use: ["file-loader"],
+         },
+         {
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
+         },
+      ],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         template: path.join(__dirname, "public/index.html"),
+      }),
+   ],
+};
